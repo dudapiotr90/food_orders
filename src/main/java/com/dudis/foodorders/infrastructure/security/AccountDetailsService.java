9d8b54cdd03d3bd95609c436bd8160a0e208f030
Piotr Duda <dudapiotr90@gmail.com>
2023-07-19 13:02:47 +0200

    LazyInit fixxed another way - entity graph

--- src/main/java/com/dudis/foodorders/infrastructure/security/AccountDetailsService.java
+++ src/main/java/com/dudis/foodorders/infrastructure/security/AccountDetailsService.java
@@ -1,11 +1,20 @@
 package com.dudis.foodorders.infrastructure.security;
 
+import com.dudis.foodorders.infrastructure.security.entity.AccountEntity;
+import com.dudis.foodorders.infrastructure.security.entity.ApiRoleEntity;
+import com.dudis.foodorders.infrastructure.security.repository.jpa.AccountJpaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import com.dudis.foodorders.infrastructure.security.repository.jpa.AccountJpaRepository;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -15,9 +24,29 @@ public class AccountDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        return accountJpaRepository.findByLogin(login)
+        AccountEntity account = accountJpaRepository.findByLogin(login)
             .orElseThrow(() -> new UsernameNotFoundException(
-                String.format("Account with login: [%s] not found",login)
+                String.format("Account with login: [%s] not found", login)
             ));
+        Collection<? extends GrantedAuthority> authority = account.getAuthorities();
+        return new User(
+            account.getUsername(),
+            account.getPassword(),
+            account.isEnabled(),
+            account.isAccountNonExpired(),
+            account.isCredentialsNonExpired(),
+            account.isAccountNonLocked(),
+            authority);
     }
+
+//    private UserDetails createAccountAccess(AccountEntity account, Set<SimpleGrantedAuthority> authorities) {
+//        return new User(account.getLogin(),account.getPassword(),account.getEnabled(),true,)
+//    }
+//
+//    private Set<SimpleGrantedAuthority> findAccountAuthority(Set<ApiRoleEntity> roles) {
+//        return roles.stream()
+//            .map(role->new SimpleGrantedAuthority(role.getRole()))
+//            .collect(Collectors.toSet());
+//    }
+
 }
