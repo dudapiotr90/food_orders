b81c22ffb09a4862f9f7d55deffcfbfb25915f30
Piotr Duda <dudapiotr90@gmail.com>
2023-07-24 11:32:55 +0200

    removed unnecessary tables, refactor entities to match, refactor food mappers

--- src/main/resources/db/migration/V1_11__init_food_orders_food.sql
+++ src/main/resources/db/migration/V1_11__init_food_orders_food.sql
@@ -1,15 +1,12 @@
 CREATE TABLE food
 (
     food_id         SERIAL          NOT NULL,
-    name            VARCHAR(128)    NOT NULL,
+    name            VARCHAR(64)     NOT NULL,
     description     TEXT            NOT NULL,
     price           NUMERIC(6,2)    NOT NULL,
-    food_type_id    INT             NOT NULL,
+    food_type       VARCHAR(64)     NOT NULL,
     menu_id         INT             NOT NULL,
     PRIMARY KEY (food_id),
-    CONSTRAINT  fk_food_food_type_id
-        FOREIGN KEY (food_type_id)
-            REFERENCES  food_type (food_type_id),
     CONSTRAINT  fk_food_menu
         FOREIGN KEY (menu_id)
             REFERENCES  menu (menu_id)
