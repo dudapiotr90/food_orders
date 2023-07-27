7c6c12e35d789c2ae79abd829ff2c2cc2b72ef45
Piotr Duda <dudapiotr90@gmail.com>
2023-07-27 22:09:24 +0200

    paginated Deliveries - finished. Fixed upload file size >1MB

--- src/main/java/com/dudis/foodorders/api/controllers/RestaurantController.java
+++ src/main/java/com/dudis/foodorders/api/controllers/RestaurantController.java
@@ -4,6 +4,7 @@ import com.dudis.foodorders.api.dtos.*;
 import com.dudis.foodorders.services.FoodService;
 import com.dudis.foodorders.services.MenuService;
 import com.dudis.foodorders.services.RestaurantService;
+import jakarta.servlet.annotation.MultipartConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 @Controller
 @RequestMapping(RestaurantController.OWNER)
@@ -24,9 +24,9 @@ public class RestaurantController {
     public static final String MANAGE = "/manage/{restaurantId}";
     public static final String MODIFY_MENU = MANAGE + "/modifyMenu";
     public static final String ADD_MENU_POSITION = MODIFY_MENU + "/add";
-    public static final String UPDATE_MENU_POSITION = MODIFY_MENU +"/updateFood";
+    public static final String UPDATE_MENU_POSITION = MODIFY_MENU + "/updateFood";
     public static final String DELETE_MENU_POSITION = MANAGE + "/deleteFood/{foodId}";
-    public static final String MANAGE_PAGE = MANAGE + "/page/{pageNumber}";
+    public static final String MANAGE_MENU_PAGE = MANAGE + "/page/{menuPageNumber}/{deliveriesPageNumber}";
     public static final String ADD_ADDRESS = MANAGE + "/addAddress";
 
     private final RestaurantService restaurantService;
@@ -39,7 +39,7 @@ public class RestaurantController {
         @PathVariable(value = "restaurantId") Integer restaurantId,
         Model modelMap
     ) {
-        return getPaginated(ownerId, restaurantId, modelMap, 1, "foodId", "asc");
+        return getPaginated(ownerId, restaurantId, modelMap, 1, "foodId", "asc", 1, "deliveryAddressId", "asc");
 
     }
 
@@ -62,8 +62,8 @@ public class RestaurantController {
         @PathVariable(value = "restaurantId") Integer restaurantId,
         @ModelAttribute("food") FoodDTO foodDTO,
         @RequestParam("image") MultipartFile image
-        ) throws IOException {
-        restaurantService.addFoodToMenu(foodDTO, restaurantId,image);
+    ) throws IOException {
+        restaurantService.addFoodToMenu(foodDTO, restaurantId, image);
         return restaurantManagerPortal(ownerId, restaurantId);
     }
 
@@ -74,7 +74,7 @@ public class RestaurantController {
         @ModelAttribute("food") FoodDTO foodDTO,
         @RequestParam("image") MultipartFile image
     ) throws IOException {
-        restaurantService.updateMenuPosition(foodDTO,restaurantId,image);
+        restaurantService.updateMenuPosition(foodDTO, restaurantId, image);
         return restaurantManagerPortal(ownerId, restaurantId);
     }
 
@@ -94,33 +94,38 @@ public class RestaurantController {
         @PathVariable(value = "restaurantId") Integer restaurantId,
         @ModelAttribute("deliveryAddress") DeliveryAddressDTO deliveryAddressDTO
     ) {
-        restaurantService.addDeliveryAddress(deliveryAddressDTO,restaurantId);
+        restaurantService.addDeliveryAddress(deliveryAddressDTO, restaurantId);
         return restaurantManagerPortal(ownerId, restaurantId);
     }
 
-    @GetMapping(value = MANAGE_PAGE)
+    @GetMapping(value = MANAGE_MENU_PAGE)
     public String getPaginated(
         @PathVariable(value = "id") Integer ownerId,
         @PathVariable(value = "restaurantId") Integer restaurantId,
         Model modelMap,
-        @PathVariable(value = "pageNumber") int pageNumber,
+        @PathVariable(value = "menuPageNumber", required = false) int menuPageNumber,
         @RequestParam("sortBy") String sortBy,
-        @RequestParam("sortHow") String sortHow
+        @RequestParam("sortHow") String sortHow,
+        @PathVariable(value = "deliveriesPageNumber", required = false) int deliveriesPageNumber,
+        @RequestParam("deliverySortBy") String deliverySortBy,
+        @RequestParam("deliverySortHow") String deliverySortHow
     ) {
         int pageSize = 4; // default Value
         RestaurantDTO restaurantDTO = restaurantService.findProcessingRestaurant(restaurantId);
-        Page<FoodDTO> menuPage = restaurantService.getPaginatedMenu(pageNumber, pageSize, sortBy, sortHow, restaurantId);
-        Page<DeliveryAddressDTO> deliveriesPage = restaurantService.getPaginatedDeliveries(pageNumber, pageSize, sortBy, sortHow, restaurantId);
-        DeliveryAddressesDTO deliveriesDTO = restaurantService.getDeliveryAddresses(restaurantId);
+        Page<FoodDTO> menuPage = restaurantService.getPaginatedMenu(menuPageNumber, pageSize, sortBy, sortHow, restaurantId);
+        Page<DeliveryAddressDTO> deliveriesPage = restaurantService.getPaginatedDeliveries(deliveriesPageNumber, pageSize, deliverySortBy, deliverySortHow, restaurantId);
+//        DeliveryAddressesDTO deliveriesDTO = restaurantService.getDeliveryAddresses(restaurantId);
         OrdersDTO ordersDTO = restaurantService.findOrders(restaurantId);
         modelMap.addAttribute("ownerId", ownerId);
         modelMap.addAttribute("restaurant", restaurantDTO);
         modelMap.addAttribute("foods", menuPage.getContent());
-        modelMap.addAttribute("deliveries", deliveriesDTO);
+        modelMap.addAttribute("deliveries", deliveriesPage.getContent());
         modelMap.addAttribute("orders", ordersDTO);
         modelMap.addAttribute("deliveryAddress", new DeliveryAddressDTO());
+        modelMap.addAttribute("menuPage", menuPageNumber);
+        modelMap.addAttribute("deliveriesPage", deliveriesPageNumber);
 
-        preparePaginatedAttributes(modelMap, menuPage, pageNumber, sortBy, sortHow);
+        preparePaginatedAttributes(modelMap, menuPage, menuPageNumber, sortBy, sortHow, deliveriesPage, deliveriesPageNumber, deliverySortBy, deliverySortHow);
 
         return "local_manager";
     }
@@ -135,13 +140,22 @@ public class RestaurantController {
         Page<FoodDTO> menuPage,
         int pageNumber,
         String sortBy,
-        String sortHow
+        String sortHow,
+        Page<DeliveryAddressDTO> deliveriesPage,
+        int deliveryPageNumber,
+        String deliverySortBy,
+        String deliverySortHow
     ) {
         modelMap.addAttribute("reverseSortHow", sortHow.equals("asc") ? "desc" : "asc");
         modelMap.addAttribute("sortHow", sortHow);
+        modelMap.addAttribute("deliverySortHow", deliverySortHow);
         modelMap.addAttribute("sortBy", sortBy);
+        modelMap.addAttribute("deliverySortBy", deliverySortBy);
         modelMap.addAttribute("currentPage", pageNumber);
-        modelMap.addAttribute("totalPages", menuPage.getTotalPages());
-        modelMap.addAttribute("totalSize", menuPage.getTotalElements());
+        modelMap.addAttribute("currentDeliveryPage", deliveryPageNumber);
+        modelMap.addAttribute("totalMenuPages", menuPage.getTotalPages());
+        modelMap.addAttribute("totalMenuSize", menuPage.getTotalElements());
+        modelMap.addAttribute("totalDeliveryPages", deliveriesPage.getTotalPages());
+        modelMap.addAttribute("totalDeliverySize", deliveriesPage.getTotalElements());
     }
 }
---
+++ src/main/resources/db/migration/V5_4__alter_food_orders_delivery_address.sql
@@ -0,0 +1,2 @@
+ALTER TABLE delivery_address ALTER COLUMN street DROP NOT NULL;
+ALTER TABLE delivery_address ALTER COLUMN postal_code DROP NOT NULL;
