1c8a74d32086baff707de08205364ecd4423c27b
Piotr Duda <dudapiotr90@gmail.com>
2023-08-08 13:06:32 +0200

    spoonacular connection configuration added contract for spoonacular - generated opeApi

---
+++ src/main/java/com/dudis/foodorders/infrastructure/configuration/BeanConfiguration.java
@@ -0,0 +1,39 @@
+package com.dudis.foodorders.infrastructure.configuration;
+
+import com.dudis.foodorders.infrastructure.spoonacular.ApiClient;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(5));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(5));
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .registerModule(new Jdk8Module())
+            .configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+
+}
---
+++ src/main/java/com/dudis/foodorders/infrastructure/configuration/WebClientConfiguration.java
@@ -0,0 +1,56 @@
+package com.dudis.foodorders.infrastructure.configuration;
+
+import com.dudis.foodorders.infrastructure.spoonacular.ApiClient;
+import com.dudis.foodorders.infrastructure.spoonacular.api.MiscApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+
+    @Value("${api.spoonacular.url}")
+    private String spoonacularUrl;
+
+    @Value("${api.spoonacular.apiKey}")
+    private String spoonacularApiKey;
+
+    @Bean
+    public WebClient spoonacularApiClient(final ObjectMapper objectMapper) {
+        final var exchangeStrategies = ExchangeStrategies
+            .builder()
+            .codecs(configurer -> {
+                configurer
+                    .defaultCodecs()
+                    .jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON));
+                configurer
+                    .defaultCodecs()
+                    .jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON));
+            })
+            .build();
+
+        return WebClient.builder()
+            .exchangeStrategies(exchangeStrategies)
+            .build();
+    }
+
+    @Bean
+    public ApiClient apiClient(final WebClient webClient) {
+        ApiClient apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(spoonacularUrl);
+        apiClient.addDefaultHeader("apiKey", spoonacularApiKey);
+        return apiClient;
+    }
+
+    @Bean
+    public MiscApi getMiscApi(final ApiClient apiClient) {
+        return new MiscApi(apiClient);
+    }
+
+}
