d423dd9641248711afd925523c3892bd151d4f07
Piotr Duda <dudapiotr90@gmail.com>
2023-08-17 16:26:38 +0200

    index controller

--- src/test/java/com/dudis/foodorders/api/controllers/HomeControllerTest.java
+++ src/test/java/com/dudis/foodorders/api/controllers/HomeControllerTest.java
@@ -1,15 +1,17 @@
 package com.dudis.foodorders.api.controllers;
 
 import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import java.util.regex.Pattern;
 
 @WebMvcTest(controllers = HomeController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -21,8 +23,13 @@ class HomeControllerTest {
     @Test
     void homeWorksCorrectly() throws Exception {
         // given, when, then
-        mockMvc.perform(get(HomeController.HOME))
-            .andExpect(status().isOk())
-            .andExpect(view().name("home"));
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("index"))
+            .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        Pattern pattern = Pattern.compile("Welcome to Food Orders Application");
+        Assertions.assertTrue(pattern.matcher(responseContent).find());
     }
 }
